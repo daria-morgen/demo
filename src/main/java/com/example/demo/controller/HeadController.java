672058988162aea643dbbf786a384e9e5a663405
daria <dk.957@ya.ru>
2019-08-16 16:30:45 +0300

    Init.

---
+++ src/main/java/com/example/demo/controller/HeadController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+/**
+ * Контроллер стартовой страницы для выбора категории продукта.
+ */
+@RequestMapping("/")
+@Controller
+public class HeadController {
+
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public String showAllQuestions(Model model) {
+
+        return "index";
+    }
+}
